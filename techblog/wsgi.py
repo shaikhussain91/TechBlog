b494a2352bf3ef520c3e7eb7c8c7e5cf37fdc07a
shaikhussain91 <shaikhussainbaba9@gmail.com>
2025-03-06 21:33:11 +0530

    new

--- techblog/wsgi.py
+++ techblog/wsgi.py
@@ -1,18 +1,6 @@
-"""
-WSGI config for techblog project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techblog.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TechBlog.settings")
 
 application = get_wsgi_application()
-
-
